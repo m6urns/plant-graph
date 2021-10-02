6490c9141697d4b6f39cc4088b5d91d82e4a3b8b
Matt Burns <m6urns@gmail.com>
2021-10-02 11:55:04 -0700

    Added date labels

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def index():
 def data():
     device_id = request.args.get('device_id', default = 'Null', type = str)
     device_data = request.args.get('device_data', default = 'Null', type = str)
-    sample_freq = request.args.get('sample_freq', default = 60, type = int)
+    sample_freq = request.args.get('sample_freq', default = 'H', type = str)
 
     ## Invalid data results in an error, need to validate URI data before running.
     if device_data == 'Null':
@@ -27,39 +27,50 @@ def data():
         n = 5
         for index in range(0, len(device_data), n):
             split_data.append(device_data[index : index + n])
+
+        num_points = len(split_data)
         
         temperature = list(map(int, [num[:2] for num in split_data]))
         moisture = list(map(int, [num[2:4] for num in split_data]))
         light = list(map(int, [num[4] for num in split_data]))
     
-        def timeRange(r1, r2):
-            return list(range(r1, r2+1))
+        #def timeRange(r1, r2):
+        #    return list(range(r1, r2+1))
 
-        r1, r2 = 1, len(split_data)
+        #r1, r2 = 1, len(split_data)
 
-        hour = list(map(str, timeRange(r1, r2)))
+        #hour = list(map(str, timeRange(r1, r2)))
 
-        num_points = len(split_data)
-    
+        days_passed = num_points / 24
+        start_date = pd.to_datetime("today") - pd.Timedelta("{}. day".format(days_passed))
+
+        date = list((map(str, pd.date_range(start_date, periods=num_points, freq=sample_freq))))
+
         graph_data = pd.DataFrame(
         {
         "Temperature": temperature,
         "Moisture": moisture,
         "Light": light,
-        "Hour": hour
+        "Date": date
         }
         )
-    
+
+        # Some kind of conditional label genarator is needed. Switch to only including date on 00:00
+        # if Date == 00:00 then Label = Date
+        # else Label = " "
+
+        graph_data["Date"] = graph_data["Date"].str.split(expand=True)
+
         temp_legend = 'Temperature'
-        temp_label = graph_data['Hour']
+        temp_label = graph_data['Date']
         temp_values = graph_data['Temperature']
 
         mos_legend = 'Moisture'
-        mos_label = graph_data['Hour']
+        mos_label = graph_data['Date']
         mos_values = graph_data['Moisture']
 
         light_legend = 'Day/Night'
-        light_label = graph_data['Hour']
+        light_label = graph_data['Date']
         light_values = graph_data['Light']
     
         return render_template('graph.html',
